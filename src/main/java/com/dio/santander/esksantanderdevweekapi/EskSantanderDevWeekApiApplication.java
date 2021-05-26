cffd78544bc3e82f5d7b1472f866069f98abb167
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-26 09:34:07 -0300

    Initial commit

---
+++ src/main/java/com/dio/santander/esksantanderdevweekapi/EskSantanderDevWeekApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.santander.esksantanderdevweekapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskSantanderDevWeekApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskSantanderDevWeekApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/santander/esksantanderdevweekapi/EskSantanderDevWeekApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santander.esksantanderdevweekapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EskSantanderDevWeekApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
