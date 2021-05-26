44bd44d4f5f40a00d7c932bacaf20916258bd4a3
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-26 16:38:39 -0300

    Apply validations

--- src/main/java/com/dio/santander/esksantanderdevweekapi/models/dto/StockDTO.java
+++ src/main/java/com/dio/santander/esksantanderdevweekapi/models/dto/StockDTO.java
@@ -1,13 +1,33 @@
 package com.dio.santander.esksantanderdevweekapi.models.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class StockDTO {
 
     private Long id;
+
+    @NotNull
+    @NotBlank
     private String name;
+
+    @NotNull
+    @DecimalMin(value = "0.00")
+    @Digits(integer = 6, fraction = 2)
     private Double price;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate date;
+
+    @NotNull
+    @DecimalMin(value = "0.00")
+    @Digits(integer = 3, fraction = 2)
     private Double variation;
 
     public Long getId() {
