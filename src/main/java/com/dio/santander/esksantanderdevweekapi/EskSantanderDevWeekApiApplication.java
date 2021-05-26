0be6f3778ef5b921bc73ff1442411e38a32e7e16
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-26 16:09:34 -0300

    Add dependency springdocs e config swagger-ui

--- src/main/java/com/dio/santander/esksantanderdevweekapi/EskSantanderDevWeekApiApplication.java
+++ src/main/java/com/dio/santander/esksantanderdevweekapi/EskSantanderDevWeekApiApplication.java
@@ -1,7 +1,11 @@
 package com.dio.santander.esksantanderdevweekapi;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EskSantanderDevWeekApiApplication {
@@ -10,4 +14,12 @@ public class EskSantanderDevWeekApiApplication {
 		SpringApplication.run(EskSantanderDevWeekApiApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("Bootcamp DIO Santander Fullstack").version("2.0")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0")
+				.url("http://springdoc.org")));
+	}
 }
