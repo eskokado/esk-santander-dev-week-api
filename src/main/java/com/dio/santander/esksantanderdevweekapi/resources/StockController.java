10f4d1ae44131e64a254a3a2f182c631c7324df1
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-27 20:35:31 -0300

    Add methos in StockController

--- src/main/java/com/dio/santander/esksantanderdevweekapi/resources/StockController.java
+++ src/main/java/com/dio/santander/esksantanderdevweekapi/resources/StockController.java
@@ -1,60 +1,54 @@
 package com.dio.santander.esksantanderdevweekapi.resources;
 
 import com.dio.santander.esksantanderdevweekapi.models.dto.StockDTO;
+import com.dio.santander.esksantanderdevweekapi.services.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/v1/stocks")
 public class StockController {
 
+    @Autowired
+    private StockService stockService;
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> save(@RequestBody @Valid  StockDTO dto) {
-        return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(stockService.save(dto));
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> update(@RequestBody @Valid StockDTO dto) {
-        return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(stockService.update(dto));
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StockDTO>> findAll() {
+
         List<StockDTO> list = new ArrayList<>();
-        StockDTO dto = new StockDTO();
-        dto.setId(1L);
-        dto.setName("Magazine Luiza");
-        dto.setPrice(100D);
-        dto.setVariation(10D);
-        dto.setDate(LocalDate.now());
-        list.add(dto);
+        list = stockService.findAll();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> findById(@PathVariable Long id) {
-        List<StockDTO> list = new ArrayList<>();
-        StockDTO dto1 = new StockDTO();
-        dto1.setId(1L);
-        dto1.setName("Magazine Luiza");
-        dto1.setPrice(100D);
-        dto1.setVariation(10D);
-        dto1.setDate(LocalDate.now());
-        list.add(dto1);
-        StockDTO dto2 = new StockDTO();
-        dto2.setId(2L);
-        dto2.setName("Ponto Frio");
-        dto2.setPrice(200D);
-        dto2.setVariation(5D);
-        dto2.setDate(LocalDate.now());
-        list.add(dto2);
-        StockDTO selected = list.stream().filter(s -> s.getId().compareTo(id) == 0).findFirst().get();
-        return ResponseEntity.ok(selected);
+        return ResponseEntity.ok(stockService.findById(id));
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> delete(@PathVariable Long id) {
+        return ResponseEntity.ok(stockService.delete(id));
+    }
+
+    @GetMapping(value = "/today", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findByToday() {
+        return ResponseEntity.ok(stockService.findByToday());
     }
 }
