bf8e8506c3801482f8ea0e3010e3f825ea8b7c1a
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-27 15:47:02 -0300

    Create mapper StockMapper

---
+++ src/main/java/com/dio/santander/esksantanderdevweekapi/mappers/StockMapper.java
@@ -0,0 +1,29 @@
+package com.dio.santander.esksantanderdevweekapi.mappers;
+
+import com.dio.santander.esksantanderdevweekapi.models.Stock;
+import com.dio.santander.esksantanderdevweekapi.models.dto.StockDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StockMapper {
+
+    public Stock toEntity(StockDTO dto) {
+        Stock stock = new Stock();
+        stock.setId(dto.getId());
+        stock.setName(dto.getName());
+        stock.setPrice(dto.getPrice());
+        stock.setVariation(dto.getVariation());
+        stock.setDate(dto.getDate());
+        return stock;
+    }
+
+    public StockDTO toDto(Stock stock) {
+        StockDTO stockDTO = new StockDTO();
+        stockDTO.setId(stock.getId());
+        stockDTO.setName(stock.getName());
+        stockDTO.setPrice(stock.getPrice());
+        stockDTO.setVariation(stock.getVariation());
+        stockDTO.setDate(stock.getDate());
+        return stockDTO;
+    }
+}
