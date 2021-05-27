2b00eef13eb31e2cf63b0642bdc7dd4da64e0b85
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-27 20:48:01 -0300

    Config description application

--- src/main/java/com/dio/santander/esksantanderdevweekapi/EskSantanderDevWeekApiApplication.java
+++ src/main/java/com/dio/santander/esksantanderdevweekapi/EskSantanderDevWeekApiApplication.java
@@ -3,6 +3,7 @@ package com.dio.santander.esksantanderdevweekapi;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,9 +16,9 @@ public class EskSantanderDevWeekApiApplication {
 	}
 
 	@Bean
-	public OpenAPI customOpenAPI() {
+	public OpenAPI customOpenAPI(@Value("${application.description}") String description) {
 		return new OpenAPI().info(new Info()
-				.title("Bootcamp DIO Santander Fullstack").version("2.0")
+				.title(description).version("2.0")
 				.termsOfService("http://swagger.io/terms")
 				.license(new License().name("Apache 2.0")
 				.url("http://springdoc.org")));
