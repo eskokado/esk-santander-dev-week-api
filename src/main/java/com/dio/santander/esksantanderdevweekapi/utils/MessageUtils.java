fb307e545963f1427d7bf238129343725b2e2157
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-27 18:37:21 -0300

    Create Exceptions

---
+++ src/main/java/com/dio/santander/esksantanderdevweekapi/utils/MessageUtils.java
@@ -0,0 +1,7 @@
+package com.dio.santander.esksantanderdevweekapi.utils;
+
+public abstract class MessageUtils {
+
+    public static final String STOCK_ALREADY_EXISTS = "Stock already exists in the database";
+
+}
